4aebcd898ce751fe0e45b2770985e864da61ac39
ijonghyeon <academy3746@gmail.com>
2023-10-27 22:02:10 +0900

    2. 2nd raw data

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  static String routeName = "/main";
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
